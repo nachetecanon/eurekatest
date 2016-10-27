8f4039348966788d4f36143d937f19b76ef2137c
icanon <icanon@paradigmadigital.com>
2016-10-27 14:42:07 +0200

    added tests

--- microservice2/src/main/java/com/bbva/microservicio2/Microservicio2.java
+++ microservice2/src/main/java/com/bbva/microservicio2/Microservicio2.java
@@ -1,16 +1,16 @@
-package com.bbva.microservicio;
+package com.bbva.microservicio2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * Created by icanon on 25/10/16.
+ * Created by icanon on 27/10/16.
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class Microservicio {
+public class Microservicio2 {
     public static void main(String[] args) {
-        SpringApplication.run(Microservicio.class, args);
+        SpringApplication.run(Microservicio2.class, args);
     }
 }
---
+++ microservice2/src/main/java/com/bbva/microservicio2/model/Customer.java
@@ -0,0 +1,52 @@
+package com.bbva.microservicio2.model;
+
+import java.util.Objects;
+
+/**
+ * Created by icanon on 27/10/16.
+ */
+public class Customer {
+    private String userId;
+    private String name;
+    private String phone;
+    private String email;
+
+    public Customer(String userId, String name, String phone, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(userId, customer.userId) &&
+                Objects.equals(name, customer.name) &&
+                Objects.equals(phone, customer.phone) &&
+                Objects.equals(email, customer.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, name, phone, email);
+    }
+}
